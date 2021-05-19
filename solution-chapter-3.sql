ed7596cbe8c40615458a0702868319a3c9514e37
Ironhack Education Team <admin.education@ironhack.com>
2021-05-19 22:25:24 +0200

    sql clauses capital letters to better distinguish

--- solution-chapter-3.sql
+++ solution-chapter-3.sql
@@ -6,12 +6,12 @@ In order to asnwer this question we need to follow the follwing steps:
 3. We need to join the output of query (2) and Apartments table. Then group by Building ID to get unique rows per Building. At the same time we need to aggregate the rows by counting the number of apartments in each building
 Combining the above we get:
 */
-select BuildingID, count(a.AptID)
-from Requests r
-inner join Apartments a
-on r.AptID = a.AptID
-where r.Status = 'Open'
-group by BuildingID;
+SELECT BuildingID, COUNT(a.AptID)
+FROM Requests r
+INNER JOIN Apartments a
+ON r.AptID = a.AptID
+WHERE r.Status = 'Open'
+GROUP BY BuildingID;
 
 /*
 Select an SQL query to get a list of tenants who are renting more than one apartment.
@@ -23,7 +23,8 @@ In order to asnwer this question we need to remember:
 
 Finally, we have only 1 option left. This query follows all the rules and executes correct statement. Hence, it is the correct answer.
 */
-Select tenantid, count(aptid) as number
-From apttenants
-Group by tenantid
-Having number>1
+
+SELECT TenantID, COUNT(AptID) AS number
+FROM AptTenants
+GROUP BY TenantID
+HAVING number > 1
